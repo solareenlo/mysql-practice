8c8e15b7f779fe32a06d32fad3d9614a6ff03fdb
solareenlo <solareenlo@gmail.com>
2019-02-25 19:21:12 +0900

    add レコードの挿入の仕方

---
+++ insert_record.sql
@@ -0,0 +1,6 @@
+insert into users (id, name, score) values (1, 'taro', 5.8);
+insert into users (id, name, score) values (2, 'sola', 8.2);
+insert into users (id, name, score) values (3, 'test', 10.1);
+insert into users (id, name, score) values (4, 'yamada', null);
+
+select * from users;

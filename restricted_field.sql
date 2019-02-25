778a7d6f207609e4fb3f7fc0433ce5636712f437
solareenlo <solareenlo@gmail.com>
2019-02-25 22:50:11 +0900

    fix typo

--- restricted_field.sql
+++ restricted_field.sql
@@ -1,10 +1,11 @@
-dorp table if exists users;
+drop table if exists users;
 create table users (
-  id int unsigned primary key auto_increme, -- 一意の値を自動的に割り当ててる.
+  id int unsigned primary key auto_increment, -- 一意の値を自動的に割り当ててる.
   name varchar(20) unique, -- 名前が重複するとエラーになって, 書き込まれない.
   -- score float not null -- nullだとエラーになって, 書き込まれない.
   score float default 0.0 -- 初期値が0.0に設定さえてる.
 );
+desc users;
 
 insert into users (id, name, score) values
   ('taro', 5.8),

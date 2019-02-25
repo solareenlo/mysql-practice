f0fe5609796e7470790c5428e4593af1ee690e0f
solareenlo <solareenlo@gmail.com>
2019-02-25 16:03:02 +0900

    SQLの外部ファイルを追加

---
+++ create_myapp.sql
@@ -0,0 +1,10 @@
+# comment
+-- comment
+/*
+comment
+comment
+comment
+*/
+drop database if exists myapp;
+create database myapp;
+grant all on myapp.* to myapp_user@localhost identified by '2VNAhigo@#';

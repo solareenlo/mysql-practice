a172131593e0fe44f44a5c239e9622e4329621ce
solareenlo <solareenlo@gmail.com>
2019-02-27 02:31:58 +0900

    fix

--- enum.sql
+++ enum.sql
@@ -4,6 +4,7 @@ create table users (
   name varchar(20),
   score float,
   rank enum('gold', 'silver', 'bronze')
+  -- goldは1, silverは2, bronzeは3という数値が割り当てられてる.
 );
 
 insert into users (name, score, rank) values ('taguchi', 5.8, 'silver');
@@ -17,7 +18,7 @@ select * from users;
 select * from users where rank = 'silver';
 
 -- enum型の中身には数値も割当らてる.
--- １番目が１, ２番目が２, ３番目が３...というふうに.
+-- 1番目が1, 2番目が2, 3番目が3...というふうに.
 -- なので, 上の抽出は以下のようにしてもかける.
 select * from users where rank = 2;
 -- この数値は挿入時や更新時にも使える.
